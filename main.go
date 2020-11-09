8a7f6cefefc8354fcfa7e8daec51d07ae11735f2
Mikhail Angelov <mikhail.angelov@gmail.com>
2020-11-09 13:13:36 +0300

    init-2

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	p := getEnv("HTTP_PORT", "8008")
 	s := &http.Server{
 		Addr:    fmt.Sprintf("0.0.0.0:%s", p),
-		Handler: api.GetRouter(),
+		Handler: web.GetRouter(),
 	}
 	log.Printf("Listening on %s", s.Addr)
 	log.Fatal(s.ListenAndServe())
